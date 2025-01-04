232b2bb7c36c37d5aaa99abe17726511b666186f
Jean <jeanaugustopr@gmail.com>
2025-01-04 12:02:31 -0300

    CalculoSimples.py

---
+++ CalculoSimples.py
@@ -0,0 +1,11 @@
+cod1 = int(input(""))
+num1 = int(input(""))
+valor1 = float(input(""))
+
+cod2 = int(input(""))
+num2 = int(input(""))
+valor2 = float(input(""))
+
+valorTotal = (num1 * valor1) + (num2 * valor2)
+
+print(f"VALOR A PAGAR: R$ {valorTotal:.2f}")
